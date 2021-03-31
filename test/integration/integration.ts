2b51c4a22cebcf6f4b712693327d4ae90e32e2b5
itowlson <ivan.towlson@microsoft.com>
2021-03-31 13:53:57 +1300

    Centralise test client definition

--- test/integration/integration.ts
+++ test/integration/integration.ts
@@ -5,10 +5,11 @@ import { assert } from 'chai';
 import * as bindle from '../../ts/index';
 
 const testAgent = new https.Agent({ rejectUnauthorized: false });
+const DEMO_SERVER_URL = 'https://bindle.deislabs.io/v1';
+const client = new bindle.BindleClient(DEMO_SERVER_URL, testAgent);
 
 describe("Bindle", () => {
     it("fetches the sample bindle", async () => {
-        const client = new bindle.BindleClient('https://bindle.deislabs.io/v1', testAgent);
         const invoice = await client.getInvoice('enterprise.com/warpcore/1.0.0');
         assert.equal('1.0.0', invoice.bindleVersion);
         assert.equal('enterprise.com/warpcore', invoice.bindle.name);
@@ -17,7 +18,6 @@ describe("Bindle", () => {
         assert.equal('isolinear_chip.txt', invoice.parcels[0].label.name);
     });
     it("queries the sample registry", async () => {
-        const client = new bindle.BindleClient('https://bindle.deislabs.io/v1', testAgent);
         const qr = await client.queryInvoices({ query: 'warpcore' });
         assert.equal(1, qr.total);
         assert.equal(1, qr.invoices.length);
